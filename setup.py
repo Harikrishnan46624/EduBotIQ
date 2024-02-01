9c6efbdf065d4847b0bc19304dcf5a0b18abf766
HARIKRISHNAN.P.S <harikrishnan46624@gmail.com>
2024-02-01 17:10:24 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'EduBotIQ Chatbot',
+    version= '0.0.0',
+    author= 'Harikrishnan',
+    author_email= 'harikrishnan46624@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
